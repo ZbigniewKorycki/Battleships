5dd3d55eb545a8bfb69c7026346e3ee5b61c63ca
szmisztal <sz.misztal@gmail.com>
2023-10-22 11:40:30 +0200

    add encode_format class variable

--- data_utils.py
+++ data_utils.py
@@ -1,12 +1,13 @@
 import json
 
 
-encode_format = "UTF-8"
-
 class DataUtils:
 
+    def __init__(self):
+        self.encode_format = "UTF-8"
+
     def serialize_to_json(self, data):
-        return json.dumps(data).encode(encode_format)
+        return json.dumps(data).encode(self.encode_format)
 
     def deserialize_json(self, data):
         return json.loads(data)
