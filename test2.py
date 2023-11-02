9e6583796908e6b8442311fdb7abf23091e9c151
szmisztal <sz.misztal@gmail.com>
2023-11-02 11:53:44 +0100

    test file to test actuall methods

---
+++ test2.py
@@ -0,0 +1,11 @@
+from board import Board
+from ship import Ship
+
+board = Board()
+ship = Ship("B", 5, 3, "vertical")
+ship_2 = Ship("D", 1, 2, "horizontal")
+# board.add_ship("A", 5, 3, "horizontal")
+board.add_ship(ship)
+board.add_ship(ship_2)
+# board.add_ship("D", 7, 4, "horizontal")
+print(board.ships.ships_list)
