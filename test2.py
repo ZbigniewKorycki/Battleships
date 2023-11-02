938fd298cb408b05f82b73156ddc9e53ae677df8
szmisztal <sz.misztal@gmail.com>
2023-11-02 16:48:42 +0100

    more tests

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ from board import Board
 from ships_logic import Ship
 
 board = Board()
-ship = Ship("G", 8, 3, "vertical")
+ship = Ship("D", 3, 4, "vertical")
 ship_2 = Ship("D", 3, 4, "horizontal")
 board.add_ship(ship)
 board.add_ship(ship_2)
