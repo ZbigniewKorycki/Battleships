6503d71ec5bbbb487899cb39a7c5b662b0a9dce9
szmisztal <sz.misztal@gmail.com>
2023-11-07 13:50:04 +0100

    my tests

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
-from board import Board
-from ships_logic import Ship
+from player import Player
 import string
 
-board = Board()
-ship = Ship("A", 1, 4, "horizontal")
-ship_2 = Ship("C", 8, 3, "horizontal")
-ship_3 = Ship("H", 4, 2, "vertical")
-ship_4 = Ship("G", 5, 3, "vertical")
-# board.add_ship(ship)
-# # board.add_ship(ship_2)
-# # board.add_ship(ship_3)
-# # board.add_ship(ship_4)
-#
+player = Player()
 
-# print(ship_2)
-print(ship_3)
-print(ship_4)
-print(ship_3.rows_list, ship_3.columns_list)
-print(ship_4.rows_list, ship_4.columns_list)
+player.coordinates_for_ship_add_to_board()
+
+
+
