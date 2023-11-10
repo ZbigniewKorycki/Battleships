fe15e27afcdcc9817dedf294381c5fa804f512ac
szmisztal <sz.misztal@gmail.com>
2023-11-10 09:25:26 +0100

    my test

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from player import Player, AIPlayer
+from ships_logic import Ship
 import string
 import random
 
 
-# player = Player()
-player_2 = AIPlayer()
-
-# player.coordinates_for_ship_add_to_board()
-player_2.coordinates_for_ship_add_to_board()
+player = Player()
+#  player_2 = AIPlayer()
+#
+player.coordinates_for_ship_add_to_board()
+# player_2.coordinates_for_ship_add_to_board()
 
