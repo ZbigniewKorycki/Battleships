5087466b9347b3ca9d4e74f1c22297ea45f2e0bf
szmisztal <sz.misztal@gmail.com>
2023-11-13 16:39:41 +0100

    my tests

--- test.py
+++ test.py
@@ -9,4 +9,6 @@ import random
 
 aiplayer = AIPlayer()
 aiplayer.coordinates_for_ship_add_to_board()
-
+ships = aiplayer.ships.ships_coordinates_on_board
+print(ships)
+print(ships["Three-masted ship"], type(ships["Four-masted ship"]))
