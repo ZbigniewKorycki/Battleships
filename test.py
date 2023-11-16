b33bad4df34728bcee790733198e729390ad5c31
szmisztal <sz.misztal@gmail.com>
2023-11-16 13:33:24 +0100

    my tests

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
 from player import Player, AIPlayer
 from ships_logic import Ship
+from board import Board
 import string
 import random
 
 
-# player = Player()
-# player.coordinates_for_ship_add_to_board()
-
-aiplayer = AIPlayer()
-aiplayer.coordinates_for_ship_add_to_board()
-ships = aiplayer.ships.ships_coordinates_on_board
-print(ships)
-print(ships["Three-masted ship"], type(ships["Four-masted ship"]))
