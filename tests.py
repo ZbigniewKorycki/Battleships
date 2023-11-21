c112f51efe74ba822d0ac0b62776cc03e4cbd8f8
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-21 16:16:54 +0100

    correct tests, give tests variables more meaningful names

--- tests.py
+++ tests.py
@@ -11,6 +11,7 @@ class TestBoard(unittest.TestCase):
 
     def test_create_starting_board(self):
         new_board = self.board.create_starting_board()
+
         self.assertIsInstance(new_board, dict)
         self.assertEqual(len(new_board), self.board.size_rows)
         self.assertEqual(len(new_board["A"]), self.board.size_columns)
@@ -18,56 +19,61 @@ class TestBoard(unittest.TestCase):
     def test_check_if_coordinate_within_board_border(self):
         # rows A-J, columns 1-10
 
-        coordinate_1 = self.board.check_if_coordinate_within_board_border({"row": "A", "column": 1})
-        coordinate_2 = self.board.check_if_coordinate_within_board_border({"row": "A", "column": 10})
-        coordinate_3 = self.board.check_if_coordinate_within_board_border({"row": "J", "column": 1})
-        coordinate_4 = self.board.check_if_coordinate_within_board_border({"row": "J", "column": 10})
-        coordinate_5 = self.board.check_if_coordinate_within_board_border({"row": "A", "column": 11})
-        coordinate_6 = self.board.check_if_coordinate_within_board_border({"row": "K", "column": 1})
-        coordinate_7 = self.board.check_if_coordinate_within_board_border({"row": "AA", "column": 1})
-        coordinate_8 = self.board.check_if_coordinate_within_board_border({"row": "a", "column": 2})
-        coordinate_9 = self.board.check_if_coordinate_within_board_border({"row": 5, "column": 5})
-        coordinate_10 = self.board.check_if_coordinate_within_board_border({"row": 0, "column": 0})
-        coordinate_11 = self.board.check_if_coordinate_within_board_border({"row": "A", "column": "A"})
-        self.assertTrue(coordinate_1)
-        self.assertTrue(coordinate_2)
-        self.assertTrue(coordinate_3)
-        self.assertTrue(coordinate_4)
-        self.assertFalse(coordinate_5)
-        self.assertFalse(coordinate_6)
-        self.assertFalse(coordinate_7)
-        self.assertFalse(coordinate_8)
-        self.assertFalse(coordinate_9)
-        self.assertFalse(coordinate_10)
-        self.assertFalse(coordinate_11)
+        coordinate_correct_first_row_first_column = self.board.check_if_coordinate_within_board_border(
+            {"row": "A", "column": 1})
+        coordinate_correct_first_row_last_column = self.board.check_if_coordinate_within_board_border(
+            {"row": "A", "column": 10})
+        coordinate_correct_last_row_first_column = self.board.check_if_coordinate_within_board_border(
+            {"row": "J", "column": 1})
+        coordinate_correct_last_row_last_column = self.board.check_if_coordinate_within_board_border(
+            {"row": "J", "column": 10})
+        coordinate_incorrect_column_of_of_range = self.board.check_if_coordinate_within_board_border(
+            {"row": "A", "column": 11})
+        coordinate_incorrect_row_of_of_range = self.board.check_if_coordinate_within_board_border(
+            {"row": "K", "column": 1})
+        coordinate_incorrect_double_row = self.board.check_if_coordinate_within_board_border({"row": "AA", "column": 1})
+        coordinate_incorrect_row_lowercase = self.board.check_if_coordinate_within_board_border(
+            {"row": "a", "column": 2})
+        coordinate_incorrect_row_as_numeric = self.board.check_if_coordinate_within_board_border(
+            {"row": 5, "column": 5})
+        coordinate_incorrect_column_as_letter = self.board.check_if_coordinate_within_board_border(
+            {"row": "A", "column": "A"})
+
+        self.assertTrue(coordinate_correct_first_row_first_column)
+        self.assertTrue(coordinate_correct_first_row_last_column)
+        self.assertTrue(coordinate_correct_last_row_first_column)
+        self.assertTrue(coordinate_correct_last_row_last_column)
+        self.assertFalse(coordinate_incorrect_column_of_of_range)
+        self.assertFalse(coordinate_incorrect_row_of_of_range)
+        self.assertFalse(coordinate_incorrect_double_row)
+        self.assertFalse(coordinate_incorrect_row_lowercase)
+        self.assertFalse(coordinate_incorrect_row_as_numeric)
+        self.assertFalse(coordinate_incorrect_column_as_letter)
 
     def test_get_row_from_index(self):
-        row_1 = self.board.get_row_from_index(1)
-        row_2 = self.board.get_row_from_index(10)
-        row_3 = self.board.get_row_from_index(0)
-        row_4 = self.board.get_row_from_index(11)
-        row_5 = self.board.get_row_from_index("A")
-        row_6 = self.board.get_row_from_index("J")
-        self.assertEqual("A", row_1)
-        self.assertEqual("J", row_2)
-        self.assertIs(row_3, None)
-        self.assertIs(row_4, None)
-        self.assertIs(row_5, None)
-        self.assertIs(row_6, None)
+        correct_index_first = self.board.get_row_from_index(1)
+        correct_index_last = self.board.get_row_from_index(10)
+        incorrect_index_out_of_range_minus = self.board.get_row_from_index(-1)
+        incorrect_index_out_of_range = self.board.get_row_from_index(11)
+        incorrect_index_letter = self.board.get_row_from_index("A")
+        self.assertEqual("A", correct_index_first)
+        self.assertEqual("J", correct_index_last)
+        self.assertIs(incorrect_index_out_of_range_minus, None)
+        self.assertIs(incorrect_index_out_of_range, None)
+        self.assertIs(incorrect_index_letter, None)
 
     def test_get_index_from_row(self):
-        index_1 = self.board.get_index_from_row(1)
-        index_2 = self.board.get_index_from_row(0)
-        index_3 = self.board.get_index_from_row("A")
-        index_4 = self.board.get_index_from_row("J")
-        index_5 = self.board.get_index_from_row("a")
-        index_6 = self.board.get_index_from_row("Z")
-        self.assertIs(index_1, None)
-        self.assertIs(index_2, None)
-        self.assertEqual(1, index_3)
-        self.assertEqual(10, index_4)
-        self.assertIs(index_5, None)
-        self.assertIs(index_6, None)
+        correct_row_a = self.board.get_index_from_row("A")
+        correct_row_j = self.board.get_index_from_row("J")
+        incorrect_row_as_number = self.board.get_index_from_row(1)
+        incorrect_row_lowercase = self.board.get_index_from_row("a")
+        incorrect_row_out_of_range = self.board.get_index_from_row("Z")
+
+        self.assertEqual(1, correct_row_a)
+        self.assertEqual(10, correct_row_j)
+        self.assertIs(incorrect_row_as_number, None)
+        self.assertIs(incorrect_row_lowercase, None)
+        self.assertIs(incorrect_row_out_of_range, None)
 
     def test_add_block_to_board(self):
         correct_coordinate = {"row": "B", "column": 5}
@@ -81,4 +87,3 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(";", self.board.get_symbol_from_player_board(correct_coordinate))
         self.assertRaises(KeyError, self.board.get_symbol_from_player_board, incorrect_coordinate_wrong_row)
         self.assertRaises(KeyError, self.board.get_symbol_from_player_board, incorrect_coordinate_wrong_column)
-
